f3e02908a4ac6d8dff2e6910920d1789901c8395
MacMini <181009546+veles-rgb@users.noreply.github.com>
2025-04-14 18:39:06 -0400

    Make fetchWeather console log
    To get API paths to data in order to get information.

--- js/modules/getWeather.js
+++ js/modules/getWeather.js
@@ -5,6 +5,7 @@ export async function fetchWeather(location, units) {
     try {
         const response = await fetch(url);
         const data = await response.json();
+        console.log(data);
         return data;
     } catch (error) {
         console.error(error);
