38102fab59c6d600adaba7ea6261ed781a46f75e
MacMini <181009546+veles-rgb@users.noreply.github.com>
2025-04-14 15:51:14 -0400

    Setup basic main js file (app.js)

--- js/app.js
+++ js/app.js
@@ -1,1 +1,4 @@
 // app.js
+import { fetchWeather } from "./modules/getWeather.js"; // Here for reference
+import "./modules/display.js";
+import * as Display from "./modules/display.js"; // Here for reference
