541ed44bf75bc90010f65c2f3a33188b265c892f
MacMini <181009546+veles-rgb@users.noreply.github.com>
2025-04-17 15:18:41 -0400

    Create fetch hourly weather function

--- js/modules/getWeather.js
+++ js/modules/getWeather.js
@@ -11,3 +11,16 @@ export async function fetchWeather(location, units) {
         console.error(error);
     }
 }
+
+export async function fetchHourlyWeather(location, units) {
+    const apiKey = "8GJRCDX98S75EPJ9H3LVZCY5F";
+    const url = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?unitGroup=${units}&include=hours&key=${apiKey}&contentType=json`;
+    try {
+        const response = await fetch(url);
+        const data = await response.json();
+        console.log(data);
+        return data;
+    } catch (error) {
+        console.error(error);
+    }
+}
