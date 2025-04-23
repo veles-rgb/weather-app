abcc944361f5fe75a727f229cd3ad5ad18d71dec
MacMini <181009546+veles-rgb@users.noreply.github.com>
2025-04-23 18:14:35 -0400

    Add background.js imports for testing animations

--- js/app.js
+++ js/app.js
@@ -2,3 +2,7 @@
 import { fetchWeather } from "./modules/getWeather.js"; // Here for reference
 import "./modules/display.js";
 import * as Display from "./modules/display.js"; // Here for reference
+
+// FOR TESTING WEATHER ANIMATIONS
+import * as Background from "./modules/background.js";
+window.Background = Background;
