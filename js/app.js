72b4d1634d6816a4bc5801725eef19e097ce63fc
MacMini <181009546+veles-rgb@users.noreply.github.com>
2025-04-29 14:49:54 -0400

    Clean up app.js and add cloud images to assets

--- js/app.js
+++ js/app.js
@@ -1,7 +1,5 @@
 // app.js
-import { fetchWeather } from "./modules/getWeather.js"; // Here for reference
 import "./modules/display.js";
-import * as Display from "./modules/display.js"; // Here for reference
 
 // FOR TESTING WEATHER ANIMATIONS
 import * as Background from "./modules/background.js";
