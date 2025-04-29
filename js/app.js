acca7362da95e744f17faaff6b6cd3e26be205ae
MacMini <181009546+veles-rgb@users.noreply.github.com>
2025-04-29 16:27:20 -0400

    Add imports for testing new loading function

--- js/app.js
+++ js/app.js
@@ -4,3 +4,6 @@ import "./modules/display.js";
 // FOR TESTING WEATHER ANIMATIONS
 import * as Background from "./modules/background.js";
 window.Background = Background;
+
+import * as Display from "./modules/display.js";
+window.Display = Display;
