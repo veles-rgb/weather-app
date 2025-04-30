040bb3e530ae23693148a62b3e9902f61cca8bea
MacMini <181009546+veles-rgb@users.noreply.github.com>
2025-04-30 14:25:34 -0400

    Change background based on imported getLocalTime

--- js/app.js
+++ js/app.js
@@ -1,9 +1,15 @@
 // app.js
 import "./modules/display.js";
+import { getLocalTime } from "./modules/backgroundDisplay.js";
+
+getLocalTime();
 
 // FOR TESTING WEATHER ANIMATIONS
 import * as Background from "./modules/background.js";
 window.Background = Background;
+
+import * as BackgroundDisplay from "./modules/backgroundDisplay.js";
+window.BackgroundDisplay = BackgroundDisplay;
 
 import * as Display from "./modules/display.js";
 window.Display = Display;
